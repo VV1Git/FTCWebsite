f1b297b8654b443ba3fa929ccf8c16213dd062ae
VV1Git <vikramaditya.vedant@gmail.com>
2023-12-16 22:40:34 -0800

    Use prod database

--- firstconnect/src/lib/pb.ts
+++ firstconnect/src/lib/pb.ts
@@ -1,7 +1,7 @@
 import PocketBase, { type AuthModel } from 'pocketbase'
 import { writable, type Writable } from 'svelte/store';
 
-export const pb = new PocketBase('http://127.0.0.1:8090');
+export const pb = new PocketBase('https://firstconnect.pockethost.io/');
 
 export let user: Writable<AuthModel> = writable();
 pb.authStore.onChange((_, v) => {
